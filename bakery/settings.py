e249a8049b156178502fb019d563c60abeb0f71b
Phuc <ngohoangphuc.work@gmail.com>
2024-01-19 20:02:14 +0700

    update

--- bakery/settings.py
+++ bakery/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import os
+
+# ...
+
+# Static files (CSS, JavaScript, images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+# Heroku: Update to allow serving static files on Heroku
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
+# Heroku: Update to allow serving static files on Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
