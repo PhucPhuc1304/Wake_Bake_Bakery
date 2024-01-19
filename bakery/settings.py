2e73af2643bb9ddcb7286354e6bade3a4b0e0f43
Phuc <ngohoangphuc.work@gmail.com>
2024-01-19 20:07:09 +0700

    Update settings.py

--- bakery/settings.py
+++ bakery/settings.py
@@ -125,8 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Heroku: Update to allow serving static files on Heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
